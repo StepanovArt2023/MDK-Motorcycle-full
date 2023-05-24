a91dcc00a4bea20ccc220fc83d76b4ce70cdf324
StepanovArt2023 <124656177+StepanovArt2023@users.noreply.github.com>
2023-05-24 17:00:14 +0300

    Update Class1.cs

--- MDK-Motorcycle/Class1.cs
+++ MDK-Motorcycle/Class1.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MDK_Motorcycle
-{
-    public static class class1
-    {
-        public static string Data = "BMW F 850 GS";
-    }
-}
